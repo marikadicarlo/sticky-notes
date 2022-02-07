4ba99ff9c28dd3863d4425defe285b621f0bca36
marikadicarlo <mdicarlo19@yahoo.com>
2022-02-07 16:24:24 -0500

    creating delete function

--- server.js
+++ server.js
@@ -1,15 +1,65 @@
-const PORT = process.envPORT || 3001;
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { deepStrictEqual } = require("assert");
 
-const express = require('express');
 const app = express();
+const port = process.env.port || 3001;
 const mainDir = path.join(__dirname, "/public");
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.get("/", function(req, res) {
-    res.sendFile(path.join(mainDir, "index.html"));
+  res.sendFile(path.join(mainDir, "index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes:id", function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(mainDir, "index.html"));
+});
+
+app.post("/api/notes", function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNote = req.body;
+  let uniqueID = savedNotes.length.toString();
+  newNote.id = uniqueID;
+  savedNotes.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  res.json(savedNotes);
+});
+
+app.delete("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+app.listen(port, function () {
+  console.log(`Now listening to port ${port}. http://localhost:${port}`);
 });
